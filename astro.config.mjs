26a0fdc36da7a74363e5f1c4b91cf26c56a03d44
TheRoyTop <royharpertop@gmail.com>
2024-04-24 22:24:34 -0600

    modified my-repo a / en astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://TheRoyTop.github.io',
-  base: 'my-repo',
+  base: '/',
   integrations: [tailwind()]
 });
